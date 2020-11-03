6a79110adbeda5f0f4273fbec0be5628d91d9cee
FragMcMags <ezragonda@gmail.com>
2020-11-03 17:27:42 -0500

    initial commit

---
+++ chapter02/concepts.py
@@ -0,0 +1,11 @@
+# Chapter 2 concepts
+x=2
+y=4
+z= x * y
+print(f"{x} * {y} = {z}")
+print("Hello python world")
+name = "Jackson"
+last_name = "Heinly"
+full_name = name + " " + last_name
+
+
---
+++ chapter02/example.py
@@ -0,0 +1,3 @@
+name = 'marcel'
+message = 'would you like coffee?'
+print(f"{name + ' ' + message}")
